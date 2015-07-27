97a8229c3c293e0312a12b028567abd70c80d06b
Rizkybayu <rizkybayu66@gmail.com>
2015-07-27 14:42:36 +0700

    fixbug

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -80,7 +80,7 @@
               <div class="kotak2">
                   <img src="{{ asset('images/news')}}/{{'cover'.$val->id}}.jpg" class="img-responsive">
                   <div class="ket">
-                  <div class="tag">{{$val->kategori}}</div>
+                  <div class="tag">{{$val->kategori->kategori}}</div>
                         <p><a href="{{ url('artikel',$val->id) }}">{{ $val->Judul }}</a></p>
                   </div>
               </div>
