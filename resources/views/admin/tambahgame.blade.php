e0c93fdb305538c117fb218975dbdafe7663767a
Rizkybayu <rizkybayu66@gmail.com>
2015-08-06 12:01:03 +0700

    penambahan fiur bilangan terbilang dan selesai alhamdulilah :)

--- resources/views/admin/tambahgame.blade.php
+++ resources/views/admin/tambahgame.blade.php
@@ -51,8 +51,9 @@
                                         </div>
                                         <div class="form-group @if ($errors->has('harga')) has-error @endif">
                                             {!! Form::label('Harga :') !!}
-                                            Rp.{!! Form::text('harga',null,['class'=>'form-control','placeholder'=>'Masukan Harga','maxlength'=>'40']) !!}
+                                            <b>Rp.<div id="terbilang"></div></b>{!! Form::text('harga',null,['class'=>'form-control','placeholder'=>'Masukan Harga','maxlength'=>'40','onkeyup'=>'terbilang()','id'=>'nominal']) !!}
                                             @if ($errors->has('harga')) <p class="help-block">{{ $errors->first('harga') }}</p> @endif
+                                        
                                         </div>                                        
                                         <div class="col-sm-3" style="clear:both">
                                         {!!Form::submit('Submit!',['class' => 'btn btn-default','name'=>'simpan']) !!}
@@ -76,4 +77,82 @@
          <!-- /. PAGE WRAPPER  -->
         </div>
      <!-- /. WRAPPER  -->
+<script charset="utf-8" type="text/javascript">
+function terbilang(){
+    var bilangan=document.getElementById("nominal").value;
+    var kalimat="";
+    var angka   = new Array('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0');
+    var kata    = new Array('','Satu','Dua','Tiga','Empat','Lima','Enam','Tujuh','Delapan','Sembilan');
+    var tingkat = new Array('','Ribu','Juta','Milyar','Triliun');
+    var panjang_bilangan = bilangan.length;
+     
+    /* pengujian panjang bilangan */
+    if(panjang_bilangan > 15){
+        kalimat = "Diluar Batas";
+    }else{
+        /* mengambil angka-angka yang ada dalam bilangan, dimasukkan ke dalam array */
+        for(i = 1; i <= panjang_bilangan; i++) {
+            angka[i] = bilangan.substr(-(i),1);
+        }
+         
+        var i = 1;
+        var j = 0;
+         
+        /* mulai proses iterasi terhadap array angka */
+        while(i <= panjang_bilangan){
+            subkalimat = "";
+            kata1 = "";
+            kata2 = "";
+            kata3 = "";
+             
+            /* untuk Ratusan */
+            if(angka[i+2] != "0"){
+                if(angka[i+2] == "1"){
+                    kata1 = "Seratus";
+                }else{
+                    kata1 = kata[angka[i+2]] + " Ratus";
+                }
+            }
+             
+            /* untuk Puluhan atau Belasan */
+            if(angka[i+1] != "0"){
+                if(angka[i+1] == "1"){
+                    if(angka[i] == "0"){
+                        kata2 = "Sepuluh";
+                    }else if(angka[i] == "1"){
+                        kata2 = "Sebelas";
+                    }else{
+                        kata2 = kata[angka[i]] + " Belas";
+                    }
+                }else{
+                    kata2 = kata[angka[i+1]] + " Puluh";
+                }
+            }
+             
+            /* untuk Satuan */
+            if (angka[i] != "0"){
+                if (angka[i+1] != "1"){
+                    kata3 = kata[angka[i]];
+                }
+            }
+             
+            /* pengujian angka apakah tidak nol semua, lalu ditambahkan tingkat */
+            if ((angka[i] != "0") || (angka[i+1] != "0") || (angka[i+2] != "0")){
+                subkalimat = kata1+" "+kata2+" "+kata3+" "+tingkat[j]+" ";
+            }
+             
+            /* gabungkan variabe sub kalimat (untuk Satu blok 3 angka) ke variabel kalimat */
+            kalimat = subkalimat + kalimat;
+            i = i + 3;
+            j = j + 1;
+        }
+         
+        /* mengganti Satu Ribu jadi Seribu jika diperlukan */
+        if ((angka[5] == "0") && (angka[6] == "0")){
+            kalimat = kalimat.replace("Satu Ribu","Seribu");
+        }
+    }
+    document.getElementById("terbilang").innerHTML=kalimat;
+}
+</script>
 @include('admin.footer')
--- resources/views/admin/ubahgame.blade.php
+++ resources/views/admin/ubahgame.blade.php
@@ -37,7 +37,8 @@
                                         </div>
                                         <div class="form-group">
                                             {!! Form::label('Ubah Harga :') !!}
-                                            {!! Form::text('harga',$edit->harga,['class'=>'form-control','placeholder'=>'Masukan Kategori','maxlength'=>'40']) !!}
+                                            <b>Rp.<div id="terbilang"></div></b>
+                                            {!! Form::text('harga',$edit->harga,['class'=>'form-control','placeholder'=>'Masukan Kategori','maxlength'=>'40','onkeyup'=>'terbilang()','id'=>'nominal']) !!}
                                         </div>
 
                                         <div class="col-sm-3" style="clear:both">
@@ -62,4 +63,82 @@
          <!-- /. PAGE WRAPPER  -->
         </div>
      <!-- /. WRAPPER  -->
+<script charset="utf-8" type="text/javascript">
+function terbilang(){
+    var bilangan=document.getElementById("nominal").value;
+    var kalimat="";
+    var angka   = new Array('0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0');
+    var kata    = new Array('','Satu','Dua','Tiga','Empat','Lima','Enam','Tujuh','Delapan','Sembilan');
+    var tingkat = new Array('','Ribu','Juta','Milyar','Triliun');
+    var panjang_bilangan = bilangan.length;
+     
+    /* pengujian panjang bilangan */
+    if(panjang_bilangan > 15){
+        kalimat = "Diluar Batas";
+    }else{
+        /* mengambil angka-angka yang ada dalam bilangan, dimasukkan ke dalam array */
+        for(i = 1; i <= panjang_bilangan; i++) {
+            angka[i] = bilangan.substr(-(i),1);
+        }
+         
+        var i = 1;
+        var j = 0;
+         
+        /* mulai proses iterasi terhadap array angka */
+        while(i <= panjang_bilangan){
+            subkalimat = "";
+            kata1 = "";
+            kata2 = "";
+            kata3 = "";
+             
+            /* untuk Ratusan */
+            if(angka[i+2] != "0"){
+                if(angka[i+2] == "1"){
+                    kata1 = "Seratus";
+                }else{
+                    kata1 = kata[angka[i+2]] + " Ratus";
+                }
+            }
+             
+            /* untuk Puluhan atau Belasan */
+            if(angka[i+1] != "0"){
+                if(angka[i+1] == "1"){
+                    if(angka[i] == "0"){
+                        kata2 = "Sepuluh";
+                    }else if(angka[i] == "1"){
+                        kata2 = "Sebelas";
+                    }else{
+                        kata2 = kata[angka[i]] + " Belas";
+                    }
+                }else{
+                    kata2 = kata[angka[i+1]] + " Puluh";
+                }
+            }
+             
+            /* untuk Satuan */
+            if (angka[i] != "0"){
+                if (angka[i+1] != "1"){
+                    kata3 = kata[angka[i]];
+                }
+            }
+             
+            /* pengujian angka apakah tidak nol semua, lalu ditambahkan tingkat */
+            if ((angka[i] != "0") || (angka[i+1] != "0") || (angka[i+2] != "0")){
+                subkalimat = kata1+" "+kata2+" "+kata3+" "+tingkat[j]+" ";
+            }
+             
+            /* gabungkan variabe sub kalimat (untuk Satu blok 3 angka) ke variabel kalimat */
+            kalimat = subkalimat + kalimat;
+            i = i + 3;
+            j = j + 1;
+        }
+         
+        /* mengganti Satu Ribu jadi Seribu jika diperlukan */
+        if ((angka[5] == "0") && (angka[6] == "0")){
+            kalimat = kalimat.replace("Satu Ribu","Seribu");
+        }
+    }
+    document.getElementById("terbilang").innerHTML=kalimat;
+}
+</script>
 @include('admin.footer')
